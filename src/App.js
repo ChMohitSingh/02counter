d7f505f3fa198bff709811e1f6eab86d1a51e31d
ChMohitSingh <mohit.singh_cs20@gla.ac.in>
2024-09-06 23:23:19 +0530

    first react program

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
+import {useState} from'react'
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  let [counter,setCounter] = useState(15)
+  // name kuch bhi ho sakte hai counter,setCounter
+  // counter variable setCounter function jo counter ko updare krne k kam aa rha hai
+
+ // let counter = 15 // ye yahi kam kr rha hai const[counter,setCounter] = useState(15)
+  const addValue = () =>{
+    console.log("Value added",counter);
+  //  console.log("Value added",Math.random())
+  
+    //counter = counter+1 // ise to jab use karege jab ese value denge let counter = 15
+    setCounter(counter+1)
+    // setCounter(counter) agr uper counter= counter+1 de rkha hao to ase bhi kr sakte hai
+  }
+
+  const removeValue = () =>{
+    setCounter(counter-1)
+  }
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+    <h1>02 counter project</h1>
+    <h2>Counter value {counter}</h2>
+    <button
+     onClick={addValue}  // onClick={addValue} used to click the butto 
+     > Add Value {counter}</button>
+
+    <br></br>
+
+    <button
+    onClick={removeValue}
+    > Remove Value {counter}</button>
+    </>
+   
+  )
 }
 
 export default App;
